b010de34d2478fa95d31142e94d561930fddde1c
Atharva Chavan <atharva.chavan907@gmail.com>
2025-10-20 23:17:43 +0530

    resolve production issue

--- components/ui/switch.jsx
+++ components/ui/switch.jsx
@@ -1,26 +1,49 @@
 "use client"
 
 import * as React from "react"
-import * as SwitchPrimitives from "@radix-ui/react-switch"
-
 import { cn } from "@/lib/utils"
 
-const Switch = React.forwardRef(({ className, ...props }, ref) => (
-  <SwitchPrimitives.Root
-    className={cn(
-      "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
-      className
-    )}
-    {...props}
-    ref={ref}
-  >
-    <SwitchPrimitives.Thumb
+const Switch = React.forwardRef(({ className, checked, onCheckedChange, ...props }, ref) => {
+  const [isOn, setIsOn] = React.useState(checked || false)
+
+  React.useEffect(() => {
+    if (checked !== undefined) {
+      setIsOn(checked)
+    }
+  }, [checked])
+
+  const handleClick = () => {
+    const newState = !isOn
+    setIsOn(newState)
+    if (onCheckedChange) {
+      onCheckedChange(newState)
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      role="switch"
+      aria-checked={isOn}
       className={cn(
-        "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
+        "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50",
+        isOn ? "bg-primary" : "bg-input",
+        className
       )}
-    />
-  </SwitchPrimitives.Root>
-))
-Switch.displayName = SwitchPrimitives.Root.displayName
+      onClick={handleClick}
+      ref={ref}
+      {...props}
+    >
+      <span
+        className={cn(
+          "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform",
+          isOn ? "translate-x-5" : "translate-x-0"
+        )}
+      />
+    </button>
+  )
+})
+
+Switch.displayName = "Switch"
 
 export { Switch }
